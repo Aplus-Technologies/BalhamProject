2e10d94707ce3d6174f257961b5592284687791a
Amy <aenco1995@gmail.com>
2022-06-03 23:27:04 +1200

    Course administrator user page UI modified

--- BalhamCollege/CourseAdministratorForm.Designer.cs
+++ BalhamCollege/CourseAdministratorForm.Designer.cs
@@ -29,88 +29,136 @@ namespace BalhamCollege
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CourseAdministratorForm));
             this.btnUpdateAssessment = new System.Windows.Forms.Button();
             this.btnDeleteAssessment = new System.Windows.Forms.Button();
             this.btnReturn = new System.Windows.Forms.Button();
             this.btnAddAssessment = new System.Windows.Forms.Button();
             this.btnProduceAssessmentsReport = new System.Windows.Forms.Button();
             this.btnEnterResult = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnUpdateAssessment
             // 
-            this.btnUpdateAssessment.Location = new System.Drawing.Point(413, 66);
+            this.btnUpdateAssessment.AutoSize = true;
+            this.btnUpdateAssessment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(94)))), ((int)(((byte)(132)))));
+            this.btnUpdateAssessment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdateAssessment.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateAssessment.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnUpdateAssessment.Location = new System.Drawing.Point(25, 342);
+            this.btnUpdateAssessment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnUpdateAssessment.Name = "btnUpdateAssessment";
-            this.btnUpdateAssessment.Size = new System.Drawing.Size(200, 60);
+            this.btnUpdateAssessment.Size = new System.Drawing.Size(242, 63);
             this.btnUpdateAssessment.TabIndex = 36;
             this.btnUpdateAssessment.Text = "Update Assessment";
-            this.btnUpdateAssessment.UseVisualStyleBackColor = true;
+            this.btnUpdateAssessment.UseVisualStyleBackColor = false;
             this.btnUpdateAssessment.Click += new System.EventHandler(this.btnUpdateAssessment_Click);
             // 
             // btnDeleteAssessment
             // 
-            this.btnDeleteAssessment.Location = new System.Drawing.Point(187, 142);
+            this.btnDeleteAssessment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(94)))), ((int)(((byte)(132)))));
+            this.btnDeleteAssessment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeleteAssessment.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteAssessment.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnDeleteAssessment.Location = new System.Drawing.Point(25, 409);
+            this.btnDeleteAssessment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDeleteAssessment.Name = "btnDeleteAssessment";
-            this.btnDeleteAssessment.Size = new System.Drawing.Size(200, 60);
+            this.btnDeleteAssessment.Size = new System.Drawing.Size(242, 65);
             this.btnDeleteAssessment.TabIndex = 35;
             this.btnDeleteAssessment.Text = "Delete Assessment";
-            this.btnDeleteAssessment.UseVisualStyleBackColor = true;
+            this.btnDeleteAssessment.UseVisualStyleBackColor = false;
             this.btnDeleteAssessment.Click += new System.EventHandler(this.btnDeleteAssessment_Click);
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(321, 329);
+            this.btnReturn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(94)))), ((int)(((byte)(132)))));
+            this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReturn.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnReturn.Location = new System.Drawing.Point(25, 621);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(161, 56);
+            this.btnReturn.Size = new System.Drawing.Size(242, 71);
             this.btnReturn.TabIndex = 34;
             this.btnReturn.Text = "Return";
-            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.UseVisualStyleBackColor = false;
             this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // btnAddAssessment
             // 
-            this.btnAddAssessment.Location = new System.Drawing.Point(187, 66);
+            this.btnAddAssessment.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(94)))), ((int)(((byte)(132)))));
+            this.btnAddAssessment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddAssessment.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddAssessment.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnAddAssessment.Location = new System.Drawing.Point(25, 269);
+            this.btnAddAssessment.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAddAssessment.Name = "btnAddAssessment";
-            this.btnAddAssessment.Size = new System.Drawing.Size(200, 60);
+            this.btnAddAssessment.Size = new System.Drawing.Size(242, 69);
             this.btnAddAssessment.TabIndex = 33;
             this.btnAddAssessment.Text = "Add Assessment";
-            this.btnAddAssessment.UseVisualStyleBackColor = true;
+            this.btnAddAssessment.UseVisualStyleBackColor = false;
             this.btnAddAssessment.Click += new System.EventHandler(this.btnAddAssessment_Click);
             // 
             // btnProduceAssessmentsReport
             // 
-            this.btnProduceAssessmentsReport.Location = new System.Drawing.Point(413, 142);
+            this.btnProduceAssessmentsReport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(94)))), ((int)(((byte)(132)))));
+            this.btnProduceAssessmentsReport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProduceAssessmentsReport.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProduceAssessmentsReport.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnProduceAssessmentsReport.Location = new System.Drawing.Point(25, 550);
+            this.btnProduceAssessmentsReport.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnProduceAssessmentsReport.Name = "btnProduceAssessmentsReport";
-            this.btnProduceAssessmentsReport.Size = new System.Drawing.Size(200, 60);
+            this.btnProduceAssessmentsReport.Size = new System.Drawing.Size(242, 67);
             this.btnProduceAssessmentsReport.TabIndex = 32;
             this.btnProduceAssessmentsReport.Text = "Produce Assessments Report";
-            this.btnProduceAssessmentsReport.UseVisualStyleBackColor = true;
+            this.btnProduceAssessmentsReport.UseVisualStyleBackColor = false;
             this.btnProduceAssessmentsReport.Click += new System.EventHandler(this.btnProduceAssessmentsReport_Click);
             // 
             // btnEnterResult
             // 
-            this.btnEnterResult.Location = new System.Drawing.Point(304, 218);
+            this.btnEnterResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(94)))), ((int)(((byte)(132)))));
+            this.btnEnterResult.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnterResult.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnterResult.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnEnterResult.Location = new System.Drawing.Point(25, 478);
+            this.btnEnterResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEnterResult.Name = "btnEnterResult";
-            this.btnEnterResult.Size = new System.Drawing.Size(200, 60);
+            this.btnEnterResult.Size = new System.Drawing.Size(242, 68);
             this.btnEnterResult.TabIndex = 31;
             this.btnEnterResult.Text = "Enter Result";
-            this.btnEnterResult.UseVisualStyleBackColor = true;
+            this.btnEnterResult.UseVisualStyleBackColor = false;
             this.btnEnterResult.Click += new System.EventHandler(this.btnEnterResult_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BalhamCollege.Properties.Resources.courseAdminPage;
+            this.pictureBox1.Location = new System.Drawing.Point(-6, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1116, 710);
+            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabStop = false;
             // 
             // CourseAdministratorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1104, 703);
             this.Controls.Add(this.btnUpdateAssessment);
             this.Controls.Add(this.btnDeleteAssessment);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.btnAddAssessment);
             this.Controls.Add(this.btnProduceAssessmentsReport);
             this.Controls.Add(this.btnEnterResult);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "CourseAdministratorForm";
-            this.Text = "CourseAdministratorForm";
+            this.Text = "Course Administrator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -122,5 +170,6 @@ namespace BalhamCollege
         private System.Windows.Forms.Button btnAddAssessment;
         private System.Windows.Forms.Button btnProduceAssessmentsReport;
         private System.Windows.Forms.Button btnEnterResult;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
