8af90605c91255cee5aebdbe27746c9ef78f5a0a
jakamum <jakamum@gmail.com>
2022-06-03 18:54:07 +1200

    All eight forms

---
+++ BalhamCollege/DeleteLecturerForm.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace BalhamCollege
+{
+    partial class DeleteLecturerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnDeleteLecturer = new System.Windows.Forms.Button();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtSuburb = new System.Windows.Forms.TextBox();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtLecturerID = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblSuburb = new System.Windows.Forms.Label();
+            this.lblStreetAddress = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblLecturerID = new System.Windows.Forms.Label();
+            this.lstLecturers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 20);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Please select a lecturer:";
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(608, 290);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(157, 42);
+            this.btnReturn.TabIndex = 57;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteLecturer
+            // 
+            this.btnDeleteLecturer.Location = new System.Drawing.Point(267, 290);
+            this.btnDeleteLecturer.Name = "btnDeleteLecturer";
+            this.btnDeleteLecturer.Size = new System.Drawing.Size(188, 42);
+            this.btnDeleteLecturer.TabIndex = 56;
+            this.btnDeleteLecturer.Text = "Delete Lecturer";
+            this.btnDeleteLecturer.UseVisualStyleBackColor = true;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(379, 225);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(221, 26);
+            this.txtCity.TabIndex = 55;
+            // 
+            // txtSuburb
+            // 
+            this.txtSuburb.Location = new System.Drawing.Point(379, 193);
+            this.txtSuburb.Name = "txtSuburb";
+            this.txtSuburb.Size = new System.Drawing.Size(221, 26);
+            this.txtSuburb.TabIndex = 54;
+            // 
+            // txtStreetAddress
+            // 
+            this.txtStreetAddress.Location = new System.Drawing.Point(379, 161);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(386, 26);
+            this.txtStreetAddress.TabIndex = 53;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(379, 129);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(221, 26);
+            this.txtFirstName.TabIndex = 52;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(379, 97);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(221, 26);
+            this.txtLastName.TabIndex = 51;
+            // 
+            // txtLecturerID
+            // 
+            this.txtLecturerID.Location = new System.Drawing.Point(379, 65);
+            this.txtLecturerID.Name = "txtLecturerID";
+            this.txtLecturerID.Size = new System.Drawing.Size(39, 26);
+            this.txtLecturerID.TabIndex = 50;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(344, 228);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(39, 20);
+            this.lblCity.TabIndex = 49;
+            this.lblCity.Text = "City:";
+            // 
+            // lblSuburb
+            // 
+            this.lblSuburb.AutoSize = true;
+            this.lblSuburb.Location = new System.Drawing.Point(318, 196);
+            this.lblSuburb.Name = "lblSuburb";
+            this.lblSuburb.Size = new System.Drawing.Size(65, 20);
+            this.lblSuburb.TabIndex = 48;
+            this.lblSuburb.Text = "Suburb:";
+            // 
+            // lblStreetAddress
+            // 
+            this.lblStreetAddress.AutoSize = true;
+            this.lblStreetAddress.Location = new System.Drawing.Point(263, 164);
+            this.lblStreetAddress.Name = "lblStreetAddress";
+            this.lblStreetAddress.Size = new System.Drawing.Size(120, 20);
+            this.lblStreetAddress.TabIndex = 47;
+            this.lblStreetAddress.Text = "Street Address:";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(292, 132);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(90, 20);
+            this.lblFirstName.TabIndex = 46;
+            this.lblFirstName.Text = "First Name:";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(293, 100);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(90, 20);
+            this.lblLastName.TabIndex = 45;
+            this.lblLastName.Text = "Last Name:";
+            // 
+            // lblLecturerID
+            // 
+            this.lblLecturerID.AutoSize = true;
+            this.lblLecturerID.Location = new System.Drawing.Point(292, 68);
+            this.lblLecturerID.Name = "lblLecturerID";
+            this.lblLecturerID.Size = new System.Drawing.Size(93, 20);
+            this.lblLecturerID.TabIndex = 44;
+            this.lblLecturerID.Text = "Lecturer ID:";
+            // 
+            // lstLecturers
+            // 
+            this.lstLecturers.FormattingEnabled = true;
+            this.lstLecturers.ItemHeight = 20;
+            this.lstLecturers.Location = new System.Drawing.Point(36, 65);
+            this.lstLecturers.Name = "lstLecturers";
+            this.lstLecturers.Size = new System.Drawing.Size(200, 204);
+            this.lstLecturers.TabIndex = 43;
+            // 
+            // DeleteLecturerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 389);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnDeleteLecturer);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtSuburb);
+            this.Controls.Add(this.txtStreetAddress);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtLecturerID);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblSuburb);
+            this.Controls.Add(this.lblStreetAddress);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblLecturerID);
+            this.Controls.Add(this.lstLecturers);
+            this.Name = "DeleteLecturerForm";
+            this.Text = "DeleteLecturerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnDeleteLecturer;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtSuburb;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtLecturerID;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblSuburb;
+        private System.Windows.Forms.Label lblStreetAddress;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblLecturerID;
+        private System.Windows.Forms.ListBox lstLecturers;
+    }
+}
---
+++ BalhamCollege/DeleteResearchTopicForm.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace BalhamCollege
+{
+    partial class DeleteResearchTopicForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTopicDescription = new System.Windows.Forms.TextBox();
+            this.lblTopicDescription = new System.Windows.Forms.Label();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnDeleteResearchTopic = new System.Windows.Forms.Button();
+            this.txtImpact = new System.Windows.Forms.TextBox();
+            this.txtResearchTopicID = new System.Windows.Forms.TextBox();
+            this.lblImpact = new System.Windows.Forms.Label();
+            this.lblResearchTopicID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstResearchTopics = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtTopicDescription
+            // 
+            this.txtTopicDescription.Location = new System.Drawing.Point(218, 254);
+            this.txtTopicDescription.Name = "txtTopicDescription";
+            this.txtTopicDescription.Size = new System.Drawing.Size(244, 26);
+            this.txtTopicDescription.TabIndex = 162;
+            // 
+            // lblTopicDescription
+            // 
+            this.lblTopicDescription.AutoSize = true;
+            this.lblTopicDescription.Location = new System.Drawing.Point(88, 257);
+            this.lblTopicDescription.Name = "lblTopicDescription";
+            this.lblTopicDescription.Size = new System.Drawing.Size(135, 20);
+            this.lblTopicDescription.TabIndex = 161;
+            this.lblTopicDescription.Text = "Topic Description:";
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(486, 337);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(157, 42);
+            this.btnReturn.TabIndex = 160;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteResearchTopic
+            // 
+            this.btnDeleteResearchTopic.Location = new System.Drawing.Point(39, 337);
+            this.btnDeleteResearchTopic.Name = "btnDeleteResearchTopic";
+            this.btnDeleteResearchTopic.Size = new System.Drawing.Size(188, 42);
+            this.btnDeleteResearchTopic.TabIndex = 159;
+            this.btnDeleteResearchTopic.Text = "Delete Research Topic";
+            this.btnDeleteResearchTopic.UseVisualStyleBackColor = true;
+            // 
+            // txtImpact
+            // 
+            this.txtImpact.Location = new System.Drawing.Point(218, 286);
+            this.txtImpact.Name = "txtImpact";
+            this.txtImpact.Size = new System.Drawing.Size(22, 26);
+            this.txtImpact.TabIndex = 158;
+            // 
+            // txtResearchTopicID
+            // 
+            this.txtResearchTopicID.Location = new System.Drawing.Point(218, 222);
+            this.txtResearchTopicID.Name = "txtResearchTopicID";
+            this.txtResearchTopicID.Size = new System.Drawing.Size(38, 26);
+            this.txtResearchTopicID.TabIndex = 157;
+            // 
+            // lblImpact
+            // 
+            this.lblImpact.AutoSize = true;
+            this.lblImpact.Location = new System.Drawing.Point(160, 289);
+            this.lblImpact.Name = "lblImpact";
+            this.lblImpact.Size = new System.Drawing.Size(62, 20);
+            this.lblImpact.TabIndex = 156;
+            this.lblImpact.Text = "Impact:";
+            // 
+            // lblResearchTopicID
+            // 
+            this.lblResearchTopicID.AutoSize = true;
+            this.lblResearchTopicID.Location = new System.Drawing.Point(76, 225);
+            this.lblResearchTopicID.Name = "lblResearchTopicID";
+            this.lblResearchTopicID.Size = new System.Drawing.Size(145, 20);
+            this.lblResearchTopicID.TabIndex = 155;
+            this.lblResearchTopicID.Text = "Research Topic ID:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 154;
+            this.label1.Text = "Please select a research topic:";
+            // 
+            // lstResearchTopics
+            // 
+            this.lstResearchTopics.FormattingEnabled = true;
+            this.lstResearchTopics.ItemHeight = 20;
+            this.lstResearchTopics.Location = new System.Drawing.Point(36, 44);
+            this.lstResearchTopics.Name = "lstResearchTopics";
+            this.lstResearchTopics.Size = new System.Drawing.Size(607, 164);
+            this.lstResearchTopics.TabIndex = 153;
+            // 
+            // DeleteResearchTopicForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 414);
+            this.Controls.Add(this.txtTopicDescription);
+            this.Controls.Add(this.lblTopicDescription);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnDeleteResearchTopic);
+            this.Controls.Add(this.txtImpact);
+            this.Controls.Add(this.txtResearchTopicID);
+            this.Controls.Add(this.lblImpact);
+            this.Controls.Add(this.lblResearchTopicID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstResearchTopics);
+            this.Name = "DeleteResearchTopicForm";
+            this.Text = "DeleteResearchTopicForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTopicDescription;
+        private System.Windows.Forms.Label lblTopicDescription;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnDeleteResearchTopic;
+        private System.Windows.Forms.TextBox txtImpact;
+        private System.Windows.Forms.TextBox txtResearchTopicID;
+        private System.Windows.Forms.Label lblImpact;
+        private System.Windows.Forms.Label lblResearchTopicID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstResearchTopics;
+    }
+}
