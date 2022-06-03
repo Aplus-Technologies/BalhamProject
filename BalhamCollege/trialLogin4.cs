a98628f0995ee5b5f1e6b239dfacc85876aed776
Amy <aenco1995@gmail.com>
2022-06-03 17:28:48 +1200

    let's see if this works, login form trial 4

---
+++ BalhamCollege/trialLogin4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BalhamCollege
+{
+    public partial class trialLogin4 : Form
+    {
+        public trialLogin4()
+        {
+            InitializeComponent();
+        }
+    }
+}
