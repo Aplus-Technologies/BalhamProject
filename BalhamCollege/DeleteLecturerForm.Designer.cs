96cd9f07c93d565517c69f42b04488fcbe6f9f42
Amy <aenco1995@gmail.com>
2022-06-06 20:59:59 +1200

    Modified Delete Lecturer form, delete function from database is functional -Amy

--- BalhamCollege/DeleteLecturerForm.Designer.cs
+++ BalhamCollege/DeleteLecturerForm.Designer.cs
@@ -29,6 +29,7 @@ namespace BalhamCollege
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.btnReturn = new System.Windows.Forms.Button();
             this.btnDeleteLecturer = new System.Windows.Forms.Button();
@@ -45,145 +46,185 @@ namespace BalhamCollege
             this.lblLastName = new System.Windows.Forms.Label();
             this.lblLecturerID = new System.Windows.Forms.Label();
             this.lstLecturers = new System.Windows.Forms.ListBox();
+            this.dsBalhamCollegeAzure = new BalhamCollege.dsBalhamCollegeAzure();
+            this.lECTURERBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.lECTURERTableAdapter = new BalhamCollege.dsBalhamCollegeAzureTableAdapters.LECTURERTableAdapter();
+            this.tableAdapterManager = new BalhamCollege.dsBalhamCollegeAzureTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.dsBalhamCollegeAzure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lECTURERBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 44);
+            this.label1.Location = new System.Drawing.Point(34, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 20);
+            this.label1.Size = new System.Drawing.Size(174, 19);
             this.label1.TabIndex = 58;
             this.label1.Text = "Please select a lecturer:";
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(608, 290);
+            this.btnReturn.Location = new System.Drawing.Point(608, 275);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(157, 42);
+            this.btnReturn.Size = new System.Drawing.Size(158, 39);
             this.btnReturn.TabIndex = 57;
             this.btnReturn.Text = "Return";
             this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // btnDeleteLecturer
             // 
-            this.btnDeleteLecturer.Location = new System.Drawing.Point(267, 290);
+            this.btnDeleteLecturer.Location = new System.Drawing.Point(267, 275);
             this.btnDeleteLecturer.Name = "btnDeleteLecturer";
-            this.btnDeleteLecturer.Size = new System.Drawing.Size(188, 42);
+            this.btnDeleteLecturer.Size = new System.Drawing.Size(188, 39);
             this.btnDeleteLecturer.TabIndex = 56;
             this.btnDeleteLecturer.Text = "Delete Lecturer";
             this.btnDeleteLecturer.UseVisualStyleBackColor = true;
+            this.btnDeleteLecturer.Click += new System.EventHandler(this.btnDeleteLecturer_Click);
             // 
             // txtCity
             // 
-            this.txtCity.Location = new System.Drawing.Point(379, 225);
+            this.txtCity.Location = new System.Drawing.Point(380, 213);
             this.txtCity.Name = "txtCity";
-            this.txtCity.Size = new System.Drawing.Size(221, 26);
+            this.txtCity.Size = new System.Drawing.Size(222, 27);
             this.txtCity.TabIndex = 55;
             // 
             // txtSuburb
             // 
-            this.txtSuburb.Location = new System.Drawing.Point(379, 193);
+            this.txtSuburb.Location = new System.Drawing.Point(380, 183);
             this.txtSuburb.Name = "txtSuburb";
-            this.txtSuburb.Size = new System.Drawing.Size(221, 26);
+            this.txtSuburb.Size = new System.Drawing.Size(222, 27);
             this.txtSuburb.TabIndex = 54;
             // 
             // txtStreetAddress
             // 
-            this.txtStreetAddress.Location = new System.Drawing.Point(379, 161);
+            this.txtStreetAddress.Location = new System.Drawing.Point(380, 153);
             this.txtStreetAddress.Name = "txtStreetAddress";
-            this.txtStreetAddress.Size = new System.Drawing.Size(386, 26);
+            this.txtStreetAddress.Size = new System.Drawing.Size(386, 27);
             this.txtStreetAddress.TabIndex = 53;
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(379, 129);
+            this.txtFirstName.Location = new System.Drawing.Point(380, 123);
             this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(221, 26);
+            this.txtFirstName.Size = new System.Drawing.Size(222, 27);
             this.txtFirstName.TabIndex = 52;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(379, 97);
+            this.txtLastName.Location = new System.Drawing.Point(380, 92);
             this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(221, 26);
+            this.txtLastName.Size = new System.Drawing.Size(222, 27);
             this.txtLastName.TabIndex = 51;
             // 
             // txtLecturerID
             // 
-            this.txtLecturerID.Location = new System.Drawing.Point(379, 65);
+            this.txtLecturerID.Location = new System.Drawing.Point(380, 61);
             this.txtLecturerID.Name = "txtLecturerID";
-            this.txtLecturerID.Size = new System.Drawing.Size(39, 26);
+            this.txtLecturerID.Size = new System.Drawing.Size(38, 27);
             this.txtLecturerID.TabIndex = 50;
             // 
             // lblCity
             // 
             this.lblCity.AutoSize = true;
-            this.lblCity.Location = new System.Drawing.Point(344, 228);
+            this.lblCity.Location = new System.Drawing.Point(344, 216);
             this.lblCity.Name = "lblCity";
-            this.lblCity.Size = new System.Drawing.Size(39, 20);
+            this.lblCity.Size = new System.Drawing.Size(42, 19);
             this.lblCity.TabIndex = 49;
             this.lblCity.Text = "City:";
             // 
             // lblSuburb
             // 
             this.lblSuburb.AutoSize = true;
-            this.lblSuburb.Location = new System.Drawing.Point(318, 196);
+            this.lblSuburb.Location = new System.Drawing.Point(318, 186);
             this.lblSuburb.Name = "lblSuburb";
-            this.lblSuburb.Size = new System.Drawing.Size(65, 20);
+            this.lblSuburb.Size = new System.Drawing.Size(66, 19);
             this.lblSuburb.TabIndex = 48;
             this.lblSuburb.Text = "Suburb:";
             // 
             // lblStreetAddress
             // 
             this.lblStreetAddress.AutoSize = true;
-            this.lblStreetAddress.Location = new System.Drawing.Point(263, 164);
+            this.lblStreetAddress.Location = new System.Drawing.Point(262, 156);
             this.lblStreetAddress.Name = "lblStreetAddress";
-            this.lblStreetAddress.Size = new System.Drawing.Size(120, 20);
+            this.lblStreetAddress.Size = new System.Drawing.Size(118, 19);
             this.lblStreetAddress.TabIndex = 47;
             this.lblStreetAddress.Text = "Street Address:";
             // 
             // lblFirstName
             // 
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(292, 132);
+            this.lblFirstName.Location = new System.Drawing.Point(292, 126);
             this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(90, 20);
+            this.lblFirstName.Size = new System.Drawing.Size(91, 19);
             this.lblFirstName.TabIndex = 46;
             this.lblFirstName.Text = "First Name:";
             // 
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(293, 100);
+            this.lblLastName.Location = new System.Drawing.Point(292, 95);
             this.lblLastName.Name = "lblLastName";
-            this.lblLastName.Size = new System.Drawing.Size(90, 20);
+            this.lblLastName.Size = new System.Drawing.Size(89, 19);
             this.lblLastName.TabIndex = 45;
             this.lblLastName.Text = "Last Name:";
             // 
             // lblLecturerID
             // 
             this.lblLecturerID.AutoSize = true;
-            this.lblLecturerID.Location = new System.Drawing.Point(292, 68);
+            this.lblLecturerID.Location = new System.Drawing.Point(292, 64);
             this.lblLecturerID.Name = "lblLecturerID";
-            this.lblLecturerID.Size = new System.Drawing.Size(93, 20);
+            this.lblLecturerID.Size = new System.Drawing.Size(94, 19);
             this.lblLecturerID.TabIndex = 44;
             this.lblLecturerID.Text = "Lecturer ID:";
             // 
             // lstLecturers
             // 
             this.lstLecturers.FormattingEnabled = true;
-            this.lstLecturers.ItemHeight = 20;
-            this.lstLecturers.Location = new System.Drawing.Point(36, 65);
+            this.lstLecturers.ItemHeight = 19;
+            this.lstLecturers.Location = new System.Drawing.Point(36, 61);
             this.lstLecturers.Name = "lstLecturers";
-            this.lstLecturers.Size = new System.Drawing.Size(200, 204);
+            this.lstLecturers.Size = new System.Drawing.Size(200, 194);
             this.lstLecturers.TabIndex = 43;
+            this.lstLecturers.SelectedIndexChanged += new System.EventHandler(this.lstLecturers_SelectedIndexChanged);
+            // 
+            // dsBalhamCollegeAzure
+            // 
+            this.dsBalhamCollegeAzure.DataSetName = "dsBalhamCollegeAzure";
+            this.dsBalhamCollegeAzure.EnforceConstraints = false;
+            this.dsBalhamCollegeAzure.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // lECTURERBindingSource
+            // 
+            this.lECTURERBindingSource.DataMember = "LECTURER";
+            this.lECTURERBindingSource.DataSource = this.dsBalhamCollegeAzure;
+            // 
+            // lECTURERTableAdapter
+            // 
+            this.lECTURERTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.ASSESSMENTTableAdapter = null;
+            this.tableAdapterManager.ASSIGNMENTTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.COURSETableAdapter = null;
+            this.tableAdapterManager.ENROLMENTTableAdapter = null;
+            this.tableAdapterManager.ISSUETableAdapter = null;
+            this.tableAdapterManager.LECTURERTableAdapter = this.lECTURERTableAdapter;
+            this.tableAdapterManager.PROGRAMMETableAdapter = null;
+            this.tableAdapterManager.RESEARCHPROJECTTableAdapter = null;
+            this.tableAdapterManager.RESEARCHTOPICTableAdapter = null;
+            this.tableAdapterManager.RESULTTableAdapter = null;
+            this.tableAdapterManager.STUDENTTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = BalhamCollege.dsBalhamCollegeAzureTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
             // DeleteLecturerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 389);
+            this.ClientSize = new System.Drawing.Size(929, 403);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnReturn);
             this.Controls.Add(this.btnDeleteLecturer);
@@ -200,8 +241,12 @@ namespace BalhamCollege
             this.Controls.Add(this.lblLastName);
             this.Controls.Add(this.lblLecturerID);
             this.Controls.Add(this.lstLecturers);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "DeleteLecturerForm";
             this.Text = "DeleteLecturerForm";
+            this.Load += new System.EventHandler(this.DeleteLecturerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsBalhamCollegeAzure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lECTURERBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,5 +270,9 @@ namespace BalhamCollege
         private System.Windows.Forms.Label lblLastName;
         private System.Windows.Forms.Label lblLecturerID;
         private System.Windows.Forms.ListBox lstLecturers;
+        private dsBalhamCollegeAzure dsBalhamCollegeAzure;
+        private System.Windows.Forms.BindingSource lECTURERBindingSource;
+        private dsBalhamCollegeAzureTableAdapters.LECTURERTableAdapter lECTURERTableAdapter;
+        private dsBalhamCollegeAzureTableAdapters.TableAdapterManager tableAdapterManager;
     }
 }
