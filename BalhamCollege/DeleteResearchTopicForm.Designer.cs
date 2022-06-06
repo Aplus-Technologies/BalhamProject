c4ac6c0aacf475eb4fed59e41e66d606e5e2ffd3
Amy <aenco1995@gmail.com>
2022-06-06 19:58:06 +1200

    modified delete research topic form, able to delete from database and update table adapters

--- BalhamCollege/DeleteResearchTopicForm.Designer.cs
+++ BalhamCollege/DeleteResearchTopicForm.Designer.cs
@@ -29,6 +29,7 @@ namespace BalhamCollege
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.txtTopicDescription = new System.Windows.Forms.TextBox();
             this.lblTopicDescription = new System.Windows.Forms.Label();
             this.btnReturn = new System.Windows.Forms.Button();
@@ -39,97 +40,159 @@ namespace BalhamCollege
             this.lblResearchTopicID = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lstResearchTopics = new System.Windows.Forms.ListBox();
+            this.dsBalhamCollegeAzure = new BalhamCollege.dsBalhamCollegeAzure();
+            this.rESEARCHTOPICBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.rESEARCHTOPICTableAdapter = new BalhamCollege.dsBalhamCollegeAzureTableAdapters.RESEARCHTOPICTableAdapter();
+            this.tableAdapterManager = new BalhamCollege.dsBalhamCollegeAzureTableAdapters.TableAdapterManager();
+            this.rESEARCHPROJECTTableAdapter = new BalhamCollege.dsBalhamCollegeAzureTableAdapters.RESEARCHPROJECTTableAdapter();
+            this.rESEARCHPROJECTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dsBalhamCollegeAzure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rESEARCHTOPICBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rESEARCHPROJECTBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // txtTopicDescription
             // 
-            this.txtTopicDescription.Location = new System.Drawing.Point(218, 254);
+            this.txtTopicDescription.Location = new System.Drawing.Point(145, 165);
+            this.txtTopicDescription.Margin = new System.Windows.Forms.Padding(2);
             this.txtTopicDescription.Name = "txtTopicDescription";
-            this.txtTopicDescription.Size = new System.Drawing.Size(244, 26);
+            this.txtTopicDescription.Size = new System.Drawing.Size(284, 20);
             this.txtTopicDescription.TabIndex = 162;
             // 
             // lblTopicDescription
             // 
             this.lblTopicDescription.AutoSize = true;
-            this.lblTopicDescription.Location = new System.Drawing.Point(88, 257);
+            this.lblTopicDescription.Location = new System.Drawing.Point(59, 167);
+            this.lblTopicDescription.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTopicDescription.Name = "lblTopicDescription";
-            this.lblTopicDescription.Size = new System.Drawing.Size(135, 20);
+            this.lblTopicDescription.Size = new System.Drawing.Size(93, 13);
             this.lblTopicDescription.TabIndex = 161;
             this.lblTopicDescription.Text = "Topic Description:";
             // 
             // btnReturn
             // 
-            this.btnReturn.Location = new System.Drawing.Point(486, 337);
+            this.btnReturn.Location = new System.Drawing.Point(324, 219);
+            this.btnReturn.Margin = new System.Windows.Forms.Padding(2);
             this.btnReturn.Name = "btnReturn";
-            this.btnReturn.Size = new System.Drawing.Size(157, 42);
+            this.btnReturn.Size = new System.Drawing.Size(105, 27);
             this.btnReturn.TabIndex = 160;
             this.btnReturn.Text = "Return";
             this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // btnDeleteResearchTopic
             // 
-            this.btnDeleteResearchTopic.Location = new System.Drawing.Point(39, 337);
+            this.btnDeleteResearchTopic.Location = new System.Drawing.Point(26, 219);
+            this.btnDeleteResearchTopic.Margin = new System.Windows.Forms.Padding(2);
             this.btnDeleteResearchTopic.Name = "btnDeleteResearchTopic";
-            this.btnDeleteResearchTopic.Size = new System.Drawing.Size(188, 42);
+            this.btnDeleteResearchTopic.Size = new System.Drawing.Size(125, 27);
             this.btnDeleteResearchTopic.TabIndex = 159;
             this.btnDeleteResearchTopic.Text = "Delete Research Topic";
             this.btnDeleteResearchTopic.UseVisualStyleBackColor = true;
+            this.btnDeleteResearchTopic.Click += new System.EventHandler(this.btnDeleteResearchTopic_Click);
             // 
             // txtImpact
             // 
-            this.txtImpact.Location = new System.Drawing.Point(218, 286);
+            this.txtImpact.Location = new System.Drawing.Point(145, 186);
+            this.txtImpact.Margin = new System.Windows.Forms.Padding(2);
             this.txtImpact.Name = "txtImpact";
-            this.txtImpact.Size = new System.Drawing.Size(22, 26);
+            this.txtImpact.Size = new System.Drawing.Size(66, 20);
             this.txtImpact.TabIndex = 158;
             // 
             // txtResearchTopicID
             // 
-            this.txtResearchTopicID.Location = new System.Drawing.Point(218, 222);
+            this.txtResearchTopicID.Location = new System.Drawing.Point(145, 144);
+            this.txtResearchTopicID.Margin = new System.Windows.Forms.Padding(2);
             this.txtResearchTopicID.Name = "txtResearchTopicID";
-            this.txtResearchTopicID.Size = new System.Drawing.Size(38, 26);
+            this.txtResearchTopicID.Size = new System.Drawing.Size(44, 20);
             this.txtResearchTopicID.TabIndex = 157;
             // 
             // lblImpact
             // 
             this.lblImpact.AutoSize = true;
-            this.lblImpact.Location = new System.Drawing.Point(160, 289);
+            this.lblImpact.Location = new System.Drawing.Point(107, 188);
+            this.lblImpact.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblImpact.Name = "lblImpact";
-            this.lblImpact.Size = new System.Drawing.Size(62, 20);
+            this.lblImpact.Size = new System.Drawing.Size(42, 13);
             this.lblImpact.TabIndex = 156;
             this.lblImpact.Text = "Impact:";
             // 
             // lblResearchTopicID
             // 
             this.lblResearchTopicID.AutoSize = true;
-            this.lblResearchTopicID.Location = new System.Drawing.Point(76, 225);
+            this.lblResearchTopicID.Location = new System.Drawing.Point(51, 146);
+            this.lblResearchTopicID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblResearchTopicID.Name = "lblResearchTopicID";
-            this.lblResearchTopicID.Size = new System.Drawing.Size(145, 20);
+            this.lblResearchTopicID.Size = new System.Drawing.Size(100, 13);
             this.lblResearchTopicID.TabIndex = 155;
             this.lblResearchTopicID.Text = "Research Topic ID:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 23);
+            this.label1.Location = new System.Drawing.Point(23, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.Size = new System.Drawing.Size(152, 13);
             this.label1.TabIndex = 154;
             this.label1.Text = "Please select a research topic:";
             // 
             // lstResearchTopics
             // 
             this.lstResearchTopics.FormattingEnabled = true;
-            this.lstResearchTopics.ItemHeight = 20;
-            this.lstResearchTopics.Location = new System.Drawing.Point(36, 44);
+            this.lstResearchTopics.Location = new System.Drawing.Point(24, 29);
+            this.lstResearchTopics.Margin = new System.Windows.Forms.Padding(2);
             this.lstResearchTopics.Name = "lstResearchTopics";
-            this.lstResearchTopics.Size = new System.Drawing.Size(607, 164);
+            this.lstResearchTopics.ScrollAlwaysVisible = true;
+            this.lstResearchTopics.Size = new System.Drawing.Size(406, 108);
             this.lstResearchTopics.TabIndex = 153;
+            this.lstResearchTopics.SelectedIndexChanged += new System.EventHandler(this.lstResearchTopics_SelectedIndexChanged);
+            // 
+            // dsBalhamCollegeAzure
+            // 
+            this.dsBalhamCollegeAzure.DataSetName = "dsBalhamCollegeAzure";
+            this.dsBalhamCollegeAzure.EnforceConstraints = false;
+            this.dsBalhamCollegeAzure.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // rESEARCHTOPICBindingSource
+            // 
+            this.rESEARCHTOPICBindingSource.DataMember = "RESEARCHTOPIC";
+            this.rESEARCHTOPICBindingSource.DataSource = this.dsBalhamCollegeAzure;
+            // 
+            // rESEARCHTOPICTableAdapter
+            // 
+            this.rESEARCHTOPICTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.ASSESSMENTTableAdapter = null;
+            this.tableAdapterManager.ASSIGNMENTTableAdapter = null;
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.COURSETableAdapter = null;
+            this.tableAdapterManager.ENROLMENTTableAdapter = null;
+            this.tableAdapterManager.ISSUETableAdapter = null;
+            this.tableAdapterManager.LECTURERTableAdapter = null;
+            this.tableAdapterManager.PROGRAMMETableAdapter = null;
+            this.tableAdapterManager.RESEARCHPROJECTTableAdapter = this.rESEARCHPROJECTTableAdapter;
+            this.tableAdapterManager.RESEARCHTOPICTableAdapter = this.rESEARCHTOPICTableAdapter;
+            this.tableAdapterManager.RESULTTableAdapter = null;
+            this.tableAdapterManager.STUDENTTableAdapter = null;
+            this.tableAdapterManager.UpdateOrder = BalhamCollege.dsBalhamCollegeAzureTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // rESEARCHPROJECTTableAdapter
+            // 
+            this.rESEARCHPROJECTTableAdapter.ClearBeforeFill = true;
+            // 
+            // rESEARCHPROJECTBindingSource
+            // 
+            this.rESEARCHPROJECTBindingSource.DataMember = "RESEARCHPROJECT";
+            this.rESEARCHPROJECTBindingSource.DataSource = this.dsBalhamCollegeAzure;
             // 
             // DeleteResearchTopicForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 414);
+            this.ClientSize = new System.Drawing.Size(469, 289);
             this.Controls.Add(this.txtTopicDescription);
             this.Controls.Add(this.lblTopicDescription);
             this.Controls.Add(this.btnReturn);
@@ -140,8 +203,13 @@ namespace BalhamCollege
             this.Controls.Add(this.lblResearchTopicID);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lstResearchTopics);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "DeleteResearchTopicForm";
             this.Text = "DeleteResearchTopicForm";
+            this.Load += new System.EventHandler(this.DeleteResearchTopicForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsBalhamCollegeAzure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rESEARCHTOPICBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rESEARCHPROJECTBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,5 +227,11 @@ namespace BalhamCollege
         private System.Windows.Forms.Label lblResearchTopicID;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox lstResearchTopics;
+        private dsBalhamCollegeAzure dsBalhamCollegeAzure;
+        private System.Windows.Forms.BindingSource rESEARCHTOPICBindingSource;
+        private dsBalhamCollegeAzureTableAdapters.RESEARCHTOPICTableAdapter rESEARCHTOPICTableAdapter;
+        private dsBalhamCollegeAzureTableAdapters.TableAdapterManager tableAdapterManager;
+        private dsBalhamCollegeAzureTableAdapters.RESEARCHPROJECTTableAdapter rESEARCHPROJECTTableAdapter;
+        private System.Windows.Forms.BindingSource rESEARCHPROJECTBindingSource;
     }
 }
--- BalhamCollege/DeleteResearchTopicForm.cs
+++ BalhamCollege/DeleteResearchTopicForm.cs
@@ -12,9 +12,123 @@ namespace BalhamCollege
 {
     public partial class DeleteResearchTopicForm : Form
     {
-        public DeleteResearchTopicForm()
+        private DataController DC;
+        private ResearchAdministratorForm frmResearchMnu;
+        private CurrencyManager cmResearchTopic;
+
+        private DataTable dtResearchTopic2; // reference to research topic table
+        private DataView researchTopicView2; // reference to dataview for research topic table 
+
+        private string researchtopicText;
+        public DeleteResearchTopicForm(DataController dc, ResearchAdministratorForm rtmnu)
         {
             InitializeComponent();
+            DC = dc;
+            frmResearchMnu = rtmnu;
+            frmResearchMnu.Hide();
+            BindControls();
+            TableAndView(); // generate updated view and table 
+            
+        }
+
+        private void TableAndView()
+        { // generate instances of table and view for RESEARCHTOPIC table 
+            dtResearchTopic2 = dsBalhamCollegeAzure.RESEARCHTOPIC;
+            researchTopicView2 = new DataView(dtResearchTopic2);
+            researchTopicView2.Sort = "TopicID"; 
+        }
+
+        public void BindControls()
+        { // create instance of currency manager 
+            cmResearchTopic = (CurrencyManager)this.BindingContext[dsBalhamCollegeAzure, "RESEARCHTOPIC"];
+        }
+
+        private void LoadResearchTopics()
+        {
+            foreach (DataRow drResearchTopic in dtResearchTopic2.Rows)
+            { // list only research topics with an associated research project
+                DataRow[] drResearchProjects = drResearchTopic.GetChildRows(dtResearchTopic2.ChildRelations["RESEARCHPROJECT$RESEARCHTOPICRESEARCHPROJECT"]);
+                if (drResearchProjects.Length == 0)
+                {
+                    researchtopicText = "";
+                    researchtopicText += drResearchTopic["TopicID"] + ", ";
+                    researchtopicText += drResearchTopic["TopicDescription"] + "\r\n";
+
+                    lstResearchTopics.Items.Add(researchtopicText);
+                }
+            }
+        }
+        private void ClearFields()
+        { // clears the following controls 
+            txtResearchTopicID.Text = String.Empty;
+            txtTopicDescription.Text = String.Empty;
+            txtImpact.Text = String.Empty;
+        }
+
+        private void btnDeleteResearchTopic_Click(object sender, EventArgs e)
+        { 
+            DataRow deleteResearchTopicRow = dtResearchTopic2.Rows[cmResearchTopic.Position];
+            if (MessageBox.Show("Are you sure you want to delete this Research Topic?", "Warning", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                rESEARCHTOPICTableAdapter.Delete(Convert.ToInt32(txtResearchTopicID.Text), txtTopicDescription.Text, txtImpact.Text);
+              
+               this.dsBalhamCollegeAzure.AcceptChanges();
+
+                // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.RESEARCHPROJECT' table. You can move, or remove it, as needed.
+                this.rESEARCHPROJECTTableAdapter.Fill(this.dsBalhamCollegeAzure.RESEARCHPROJECT);
+                // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.RESEARCHTOPIC' table. You can move, or remove it, as needed.
+                this.rESEARCHTOPICTableAdapter.Fill(this.dsBalhamCollegeAzure.RESEARCHTOPIC);
+
+                DC.UpdateResearchTopic();
+                lstResearchTopics.Items.Clear();
+                LoadResearchTopics();
+                MessageBox.Show("Research Topic deleted successfully", "Success", MessageBoxButtons.OK);
+
+                ClearFields();
+            }
+
+        }
+
+        private void lstResearchTopics_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstResearchTopics.SelectedItem != null)
+            {
+                string researchtopic = "";
+                researchtopic = lstResearchTopics.SelectedItem.ToString();
+                string[] parts = researchtopic.Split(',');
+                int topicID = Convert.ToInt32(parts[0]);
+                cmResearchTopic.Position = researchTopicView2.Find(topicID);
+                DataRow drResearchTopic = dtResearchTopic2.Rows[cmResearchTopic.Position];
+                txtResearchTopicID.Text = drResearchTopic["TopicID"].ToString();
+                txtTopicDescription.Text = drResearchTopic["TopicDescription"].ToString();
+                txtImpact.Text = drResearchTopic["Impact"].ToString();
+            }
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            frmResearchMnu.Show();
+        }
+
+        private void rESEARCHTOPICBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.rESEARCHTOPICBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dsBalhamCollegeAzure);
+
+        }
+
+        private void DeleteResearchTopicForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.RESEARCHPROJECT' table. You can move, or remove it, as needed.
+            this.rESEARCHPROJECTTableAdapter.Fill(this.dsBalhamCollegeAzure.RESEARCHPROJECT);
+            // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.RESEARCHTOPIC' table. You can move, or remove it, as needed.
+            this.rESEARCHTOPICTableAdapter.Fill(this.dsBalhamCollegeAzure.RESEARCHTOPIC);
+          
+            ClearFields();
+            lstResearchTopics.Items.Clear(); 
+            LoadResearchTopics();
         }
     }
 }
