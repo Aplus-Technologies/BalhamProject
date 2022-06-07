5264f5b56ceeaf26522497106c04016d41ba6697
Amy <aenco1995@gmail.com>
2022-06-08 00:31:37 +1200

    created Add Programme form design only

---
+++ BalhamCollege/AddProgrammeForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace BalhamCollege
+{
+    partial class AddProgrammeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProgrammeName = new System.Windows.Forms.Label();
+            this.lblProgrammeLevel = new System.Windows.Forms.Label();
+            this.txtProgrammeName = new System.Windows.Forms.TextBox();
+            this.nudLevel = new System.Windows.Forms.NumericUpDown();
+            this.btnAddProgramme = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.lblRequiredFields = new System.Windows.Forms.Label();
+            this.lblAsterix1 = new System.Windows.Forms.Label();
+            this.lblAsterix2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProgrammeName
+            // 
+            this.lblProgrammeName.AutoSize = true;
+            this.lblProgrammeName.Location = new System.Drawing.Point(51, 70);
+            this.lblProgrammeName.Name = "lblProgrammeName";
+            this.lblProgrammeName.Size = new System.Drawing.Size(141, 18);
+            this.lblProgrammeName.TabIndex = 0;
+            this.lblProgrammeName.Text = "Programme Name:";
+            // 
+            // lblProgrammeLevel
+            // 
+            this.lblProgrammeLevel.AutoSize = true;
+            this.lblProgrammeLevel.Location = new System.Drawing.Point(143, 127);
+            this.lblProgrammeLevel.Name = "lblProgrammeLevel";
+            this.lblProgrammeLevel.Size = new System.Drawing.Size(49, 18);
+            this.lblProgrammeLevel.TabIndex = 1;
+            this.lblProgrammeLevel.Text = "Level:";
+            // 
+            // txtProgrammeName
+            // 
+            this.txtProgrammeName.Location = new System.Drawing.Point(189, 62);
+            this.txtProgrammeName.MaxLength = 50;
+            this.txtProgrammeName.Name = "txtProgrammeName";
+            this.txtProgrammeName.Size = new System.Drawing.Size(387, 26);
+            this.txtProgrammeName.TabIndex = 2;
+            // 
+            // nudLevel
+            // 
+            this.nudLevel.Location = new System.Drawing.Point(189, 125);
+            this.nudLevel.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudLevel.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudLevel.Name = "nudLevel";
+            this.nudLevel.Size = new System.Drawing.Size(69, 26);
+            this.nudLevel.TabIndex = 3;
+            this.nudLevel.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnAddProgramme
+            // 
+            this.btnAddProgramme.Location = new System.Drawing.Point(91, 221);
+            this.btnAddProgramme.Name = "btnAddProgramme";
+            this.btnAddProgramme.Size = new System.Drawing.Size(167, 39);
+            this.btnAddProgramme.TabIndex = 4;
+            this.btnAddProgramme.Text = "Add Programme";
+            this.btnAddProgramme.UseVisualStyleBackColor = true;
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(473, 221);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(167, 39);
+            this.btnReturn.TabIndex = 5;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // lblRequiredFields
+            // 
+            this.lblRequiredFields.AutoSize = true;
+            this.lblRequiredFields.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequiredFields.ForeColor = System.Drawing.Color.Red;
+            this.lblRequiredFields.Location = new System.Drawing.Point(515, 170);
+            this.lblRequiredFields.Name = "lblRequiredFields";
+            this.lblRequiredFields.Size = new System.Drawing.Size(125, 18);
+            this.lblRequiredFields.TabIndex = 28;
+            this.lblRequiredFields.Text = "*Required Fields";
+            // 
+            // lblAsterix1
+            // 
+            this.lblAsterix1.AutoSize = true;
+            this.lblAsterix1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsterix1.ForeColor = System.Drawing.Color.Red;
+            this.lblAsterix1.Location = new System.Drawing.Point(40, 62);
+            this.lblAsterix1.Name = "lblAsterix1";
+            this.lblAsterix1.Size = new System.Drawing.Size(14, 18);
+            this.lblAsterix1.TabIndex = 29;
+            this.lblAsterix1.Text = "*";
+            // 
+            // lblAsterix2
+            // 
+            this.lblAsterix2.AutoSize = true;
+            this.lblAsterix2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAsterix2.ForeColor = System.Drawing.Color.Red;
+            this.lblAsterix2.Location = new System.Drawing.Point(133, 119);
+            this.lblAsterix2.Name = "lblAsterix2";
+            this.lblAsterix2.Size = new System.Drawing.Size(14, 18);
+            this.lblAsterix2.TabIndex = 30;
+            this.lblAsterix2.Text = "*";
+            // 
+            // AddProgrammeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 295);
+            this.Controls.Add(this.lblAsterix2);
+            this.Controls.Add(this.lblAsterix1);
+            this.Controls.Add(this.lblRequiredFields);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnAddProgramme);
+            this.Controls.Add(this.nudLevel);
+            this.Controls.Add(this.txtProgrammeName);
+            this.Controls.Add(this.lblProgrammeLevel);
+            this.Controls.Add(this.lblProgrammeName);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AddProgrammeForm";
+            this.Text = "Add Programme Form";
+            ((System.ComponentModel.ISupportInitialize)(this.nudLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProgrammeName;
+        private System.Windows.Forms.Label lblProgrammeLevel;
+        private System.Windows.Forms.TextBox txtProgrammeName;
+        private System.Windows.Forms.NumericUpDown nudLevel;
+        private System.Windows.Forms.Button btnAddProgramme;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Label lblRequiredFields;
+        private System.Windows.Forms.Label lblAsterix1;
+        private System.Windows.Forms.Label lblAsterix2;
+    }
+}
---
+++ BalhamCollege/AddProgrammeForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BalhamCollege
+{
+    public partial class AddProgrammeForm : Form
+    {
+        //declare global variables
+        private DataController DC;
+        private ProgrammeAdministratorForm frmProgramAdmin; // reference to Program Administrator Form
+        public AddProgrammeForm(DataController dc, ProgrammeAdministratorForm progAdmin)
+        {
+            InitializeComponent();
+            // call the following functions upon form initialization 
+            DC = dc;
+           frmProgramAdmin = progAdmin;
+           frmProgramAdmin.Hide();
+        }
+    }
+}
