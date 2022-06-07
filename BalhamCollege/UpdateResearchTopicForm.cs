97d71661a6144e512350f78a544f86e750105de8
Svetlana <sveta2k@bk.ru>
2022-06-07 13:39:32 +1200

    Create Update REsearch Topic form

--- BalhamCollege/UpdateResearchTopicForm.cs
+++ BalhamCollege/UpdateResearchTopicForm.cs
@@ -12,9 +12,75 @@ namespace BalhamCollege
 {
     public partial class UpdateResearchTopicForm : Form
     {
-        public UpdateResearchTopicForm()
+        private DataController DC;
+        private ResearchAdministratorForm frmResearchAdministrator;
+        private CurrencyManager cmResearchTopic;
+
+        public UpdateResearchTopicForm(DataController dc, ResearchAdministratorForm researchAdministrator)
         {
             InitializeComponent();
+            DC = dc;
+            frmResearchAdministrator = researchAdministrator;
+            researchAdministrator.Hide();
+            BindControls();
+        }
+
+        public void BindControls()
+        {
+            // Set up Currency Manager
+            cmResearchTopic = (CurrencyManager)this.BindingContext[DC.dsBalhamCollegeAzure, "ResearchTopic"];
+
+            // Bind list of courses
+            lstResearchTopics.DataSource = DC.dsBalhamCollegeAzure;
+            lstResearchTopics.DisplayMember = "ResearchTopic.TopicID";
+            lstResearchTopics.ValueMember = "ResearchTopic.TopicID";
+
+            // Bind controls
+            txtTopicID.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "ResearchTopic.TopicID");
+            txtTopicDescription.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "ResearchTopic.TopicDescription");
+            cboImpact.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "ResearchTopic.Impact");
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            frmResearchAdministrator.Show();
+        }
+
+        private void lstResearchTopics_Format(object sender, ListControlConvertEventArgs e)
+        {
+            // Convert database row into listitem text
+            DataRowView researchTopicRow = (DataRowView)e.ListItem;
+            e.Value = researchTopicRow.Row["TopicID"] + " " + researchTopicRow.Row["TopicDescription"];
+        }
+
+        private void btnUpdateTopic_Click(object sender, EventArgs e)
+        {
+            DataRow updateResearchTopicRow = DC.dtResearchTopic.Rows[cmResearchTopic.Position];
+
+            // Validate the entries in the fields
+            if ((txtTopicDescription.Text == "") || (cboImpact.Text == ""))
+            {
+                MessageBox.Show("Please fill in all fields correctly", "Error");
+            }
+            else
+            {
+                if (MessageBox.Show("Are you sure you want to change the research topic's details?", "Warning", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    // Save changes
+                    updateResearchTopicRow["TopicDescription"] = txtTopicDescription.Text;
+                    updateResearchTopicRow["Impact"] = cboImpact.Text;
+
+                    cmResearchTopic.EndCurrentEdit();
+                    DC.UpdateResearchTopic();
+                    MessageBox.Show("Research topic updated successfully", "Success");
+                }
+                else
+                {
+                    // Cancel changes
+                    cmResearchTopic.CancelCurrentEdit();
+                }
+            }
         }
     }
 }
