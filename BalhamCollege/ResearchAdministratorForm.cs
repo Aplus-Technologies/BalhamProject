c2187f0821e2ff7e179ec71f7d894264b45c3141
jakamum <jakamum@gmail.com>
2022-06-08 22:58:09 +1200

    Remove duplicate line

--- BalhamCollege/ResearchAdministratorForm.cs
+++ BalhamCollege/ResearchAdministratorForm.cs
@@ -17,16 +17,10 @@ namespace BalhamCollege
         private LoginForm frmLogin;
         private DeleteResearchTopicForm frmDeleteResearchTopic; // reference to delete research topic form
         private UpdateResearchTopicForm frmUpdateResearchTopic; // reference to update research topic form
-
         private AddResearchProject frmAddResearchProject; // reference to add research project form
         private RemoveResearchProjectForm frmRemoveResearchProject; // reference to remove research project form                                                  
-
-
-        private AddResearchProject frmAddResearchProject; // reference to add research project form 
         private AddResearchTopicForm frmAddResearchTopic; // reference to add research topic form
         
-
-
         public ResearchAdministratorForm(DataController dc, LoginForm lgin)
         {
             InitializeComponent();
