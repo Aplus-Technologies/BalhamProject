79f3a8a83a3ba1807b51a42af37dae9aa54aa9c6
Svetlana <sveta2k@bk.ru>
2022-06-09 00:49:19 +1200

    Rework Update Course form

--- BalhamCollege/UpdateCourseForm.cs
+++ BalhamCollege/UpdateCourseForm.cs
@@ -12,10 +12,17 @@ namespace BalhamCollege
 {
     public partial class UpdateCourseForm : Form
     {
+        //declare global variables
         private DataController DC;
-        private ProgrammeAdministratorForm frmProgrammeAdministrator;
+        private ProgrammeAdministratorForm frmProgrammeAdministrator;  // reference to Programme Administrator Form
         private CurrencyManager cmCourse;
         private CurrencyManager cmProgramme;
+
+        private DataTable dtCourse2;
+        private DataView courseView2;
+
+        private DataTable dtProgramme2;
+        private DataView programmeView2;
 
 
         public UpdateCourseForm(DataController dc, ProgrammeAdministratorForm programmeAdministrator)
@@ -25,27 +32,49 @@ namespace BalhamCollege
             frmProgrammeAdministrator = programmeAdministrator;
             programmeAdministrator.Hide();
             BindControls();
+            TableAndView(); // generate updated table and views
         }
+
+        private void TableAndView()
+        {
+            // create updated instances for Course table and dataview for course table 
+            dtCourse2 = dsBalhamCollegeAzure.COURSE;
+            courseView2 = new DataView(dtCourse2);
+            courseView2.Sort = "CourseID";
+
+            // create updated instances for Programme table and dataview for programme table 
+            dtProgramme2= dsBalhamCollegeAzure.PROGRAMME;
+            programmeView2 = new DataView(dtProgramme2);
+            programmeView2.Sort = "ProgrammeID";
+        }
+
 
         public void BindControls()
         {
             // Set up Currency Manager
             cmCourse = (CurrencyManager)this.BindingContext[DC.dsBalhamCollegeAzure, "Course"];
             cmProgramme = (CurrencyManager)this.BindingContext[DC.dsBalhamCollegeAzure, "Programme"];
-
-            // Bind list of courses
-            lstCourses.DataSource = DC.dsBalhamCollegeAzure;
-            lstCourses.DisplayMember = "Course.CourseID";
-            lstCourses.ValueMember = "Course.CourseID";
-
-            // Bind controls at right
-            txtCourseID.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "Course.CourseID");
-            txtCourseName.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "Course.CourseName");
-            nudCredits.DataBindings.Add("Value", DC.dsBalhamCollegeAzure, "Course.Credits");
-            cboStatus.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "Course.Status");
-            nudFee.DataBindings.Add("Value", DC.dsBalhamCollegeAzure, "Course.Fee");
-            txtProgrammeName.DataBindings.Add("Text", DC.dsBalhamCollegeAzure, "Programme.ProgrammeName");
         }
+        private void LoadCourses()
+        {
+            // To load all courses
+            lstCourses.Items.Clear();
+            foreach (DataRow drCourse in dtCourse2.Rows)
+            {
+                lstCourses.Items.Add(drCourse);
+            }
+        }
+        private void ClearFields()
+        {
+            // Clear all fields
+            txtCourseID.Text = string.Empty;
+            txtCourseName.Text = string.Empty;
+            nudCredits.Text = string.Empty;
+            cboStatus.Text = string.Empty;
+            nudFee.Text = string.Empty;
+            txtProgrammeName.Text = string.Empty;
+        }
+
 
         private void btnReturn_Click(object sender, EventArgs e)
         {
@@ -56,13 +85,13 @@ namespace BalhamCollege
         private void lstCourses_Format(object sender, ListControlConvertEventArgs e)
         {
             // Convert database row into listitem text
-            DataRowView courseRow = (DataRowView)e.ListItem;
-            e.Value = courseRow.Row["CourseID"] + " " + courseRow.Row["CourseName"];
+            DataRow courseRow = (DataRow)e.ListItem;
+            e.Value = courseRow["CourseID"] + " " + courseRow["CourseName"];
         }
 
         private void btnUpdateCourse_Click(object sender, EventArgs e)
         {
-            DataRow updateCourseRow = DC.dtCourse.Rows[cmCourse.Position];
+            DataRow updateCourseRow = dtCourse2.Rows[cmCourse.Position];
 
             // Validate the entries in the fields
             if ((txtCourseName.Text == "") || (nudCredits.Text == "") || (cboStatus.Text == "") || (nudFee.Text == ""))
@@ -79,14 +108,20 @@ namespace BalhamCollege
                     updateCourseRow["Status"] = cboStatus.Text;
                     updateCourseRow["Fee"] = nudFee.Value;
 
-                    cmCourse.EndCurrentEdit();
-                    DC.UpdateCourse();
+                    this.cOURSETableAdapter.Update(updateCourseRow);
+
+                    // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.COURSE' table. You can move, or remove it, as needed.
+                    this.cOURSETableAdapter.Fill(this.dsBalhamCollegeAzure.COURSE);
+
                     MessageBox.Show("Course updated successfully", "Success");
+                    LoadCourses();
+                    ClearFields();
                 }
                 else
                 {
                     // Cancel changes
-                    cmCourse.CancelCurrentEdit();
+                    LoadCourses();
+                    ClearFields();
                 }
             }
         }
@@ -94,14 +129,23 @@ namespace BalhamCollege
         // Get Programme Name for the current Course
         private void lstCourses_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (cmCourse != null)
-            {
-                // Get whole row from the "COURSE" table for the current course
-                DataRow drCourse = DC.dtCourse.Rows[cmCourse.Position];
+            // Get whole row from the "COURSE" table for the current course
+            DataRow drCourse = (DataRow)lstCourses.SelectedItem;
+            cmCourse.Position = courseView2.Find(drCourse["CourseID"]);
 
-                // Select corresponding row in the "PROGRAMME" table
-                cmProgramme.Position = DC.programmeView.Find(drCourse["ProgrammeID"]);
-            }
+            // Select corresponding row in the "PROGRAMME" table
+            cmProgramme.Position = programmeView2.Find(drCourse["ProgrammeID"]);
+            DataRow programmeRow = dtProgramme2.Rows[cmProgramme.Position];
+
+            // To populate the following controls with their corresponding values; from Course Table 
+            txtCourseID.Text = drCourse["CourseID"].ToString();
+            txtCourseName.Text = drCourse["CourseName"].ToString();
+            nudCredits.Text = drCourse["Credits"].ToString();
+            nudCredits.Value = Convert.ToInt32(drCourse["Credits"].ToString());
+            cboStatus.Text = drCourse["Status"].ToString();
+            nudFee.Text = drCourse["Fee"].ToString();
+            nudFee.Value = Convert.ToDecimal(drCourse["Fee"].ToString());
+            txtProgrammeName.Text = programmeRow["ProgrammeName"].ToString();
         }
 
         private void cOURSEBindingNavigatorSaveItem_Click(object sender, EventArgs e)
@@ -119,6 +163,8 @@ namespace BalhamCollege
             // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.COURSE' table. You can move, or remove it, as needed.
             this.cOURSETableAdapter.Fill(this.dsBalhamCollegeAzure.COURSE);
 
+            LoadCourses();
+            ClearFields();
         }
     }
 }
