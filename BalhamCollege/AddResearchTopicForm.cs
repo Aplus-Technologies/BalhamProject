a19314277938776e8ed98ad96acd66cf365f66bf
AJ <89243448+padawan1995@users.noreply.github.com>
2022-06-08 15:29:33 +1200

    Add Research topic form functional

--- BalhamCollege/AddResearchTopicForm.cs
+++ BalhamCollege/AddResearchTopicForm.cs
@@ -12,10 +12,69 @@ namespace BalhamCollege
 {
     public partial class AddResearchTopicForm : Form
     {
-       
-        public AddResearchTopicForm()
+        //declare global variables
+        private DataController DC;
+        private ResearchAdministratorForm frmResearchAdmin; // reference to Research Administrator Form
+
+        public AddResearchTopicForm(DataController dc, ResearchAdministratorForm resAdmin)
         {
             InitializeComponent();
+            // call the following functions upon form initialization 
+            DC = dc;
+            frmResearchAdmin = resAdmin;
+            frmResearchAdmin.Hide();
         }
+
+        private void ClearFields()
+        { // Clears all controls
+            txtResearchTopicDesc.Text = "";
+            cboImpact.Text = ""; 
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            // returns user back to research administrator menu
+            this.Close(); // close current form 
+            frmResearchAdmin.Show(); // show research administrator menu 
+        }
+
+        private void rESEARCHTOPICBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.rESEARCHTOPICBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dsBalhamCollegeAzure);
+
+        }
+
+        private void AddResearchTopicForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.RESEARCHTOPIC' table. You can move, or remove it, as needed.
+            this.rESEARCHTOPICTableAdapter.Fill(this.dsBalhamCollegeAzure.RESEARCHTOPIC);
+            ClearFields(); 
+        }
+
+        private void btnAddResearchTopic_Click(object sender, EventArgs e)
+        {
+            // check if required fields are blank 
+            if ((txtResearchTopicDesc.Text == "") || (cboImpact.Text == ""))
+            {// error message
+                MessageBox.Show("Please fill in all fields correctly", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {// create new research topic record
+
+                // success message  
+
+                this.rESEARCHTOPICTableAdapter.Insert(txtResearchTopicDesc.Text, cboImpact.Text);
+
+                // TODO: This line of code loads data into the 'dsBalhamCollegeAzure.RESEARCHTOPIC' table. You can move, or remove it, as needed.
+                this.rESEARCHTOPICTableAdapter.Fill(this.dsBalhamCollegeAzure.RESEARCHTOPIC);
+
+                MessageBox.Show("Research topic added successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // controls are reset to blank 
+                ClearFields();
+
+            }
+        }    
     }
 }
